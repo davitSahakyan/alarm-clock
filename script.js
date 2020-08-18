f5d6e9d18affa1ba69951cf7a4954d9b37892e52
Davit Sahakyan <sahakyandavit92@gmail.com>
2020-08-18 18:17:55 +0400

    Add alarm part

--- script.js
+++ script.js
@@ -1,10 +1,42 @@
 const timeDiv = document.getElementById("time");
 const setTimeButton = document.getElementById("setTimeButton");
+const alarmIsSetImageContainer = document.getElementById(
+    "alarmIsSetImageContainer"
+);
+const alarmIsRingingImageContainer = document.getElementById(
+    "alarmIsRingingImageContainer"
+);
+const alarmTime = document.getElementById("alarmTime");
+let alarmNameInput = document.getElementById("alarmName");
+const alertText = document.getElementById("alertText");
 
+// time input
 let inputedHours = document.getElementById("hours");
 let inputedMinutes = document.getElementById("minutes");
 let inputedSeconds = document.getElementById("seconds");
+// alarm input
+let alarmHours = document.getElementById("alarmHours");
+let alarmMinutes = document.getElementById("alarmMinutes");
+let alarmSeconds = document.getElementById("alarmSeconds");
+//
+let clockTime;
+let createdAlarmTime;
+let alarmExists = false;
 
+const ALERT_TEXT = "You have already set alarm time";
+const ALERT_TEXT_REQUIRED = "required";
+
+// Reusable functions
+
+function addZero(time) {
+    return time < 10 ? "0" + time : time;
+}
+
+removeSpace = (string) => {
+    const stringWithoutSpaces = string.replace(/\s+/, "");
+    return stringWithoutSpaces;
+};
+// ////////////
 let currentTime = setInterval(() => {
     let date = new Date();
 
@@ -13,6 +45,19 @@ let currentTime = setInterval(() => {
     let minutes = date.getMinutes();
 
     let seconds = date.getSeconds();
+
+    // console.log(createdAlarmTime);
+    // console.log(
+    //     addZero(hours) + ":" + addZero(minutes) + ":" + addZero(seconds)
+    // );
+
+    if (
+        createdAlarmTime ===
+        addZero(hours) + ":" + addZero(minutes) + ":" + addZero(seconds)
+    ) {
+        alarmIsRingingImageContainer.style.display = "block";
+        alarmIsSetImageContainer.style.display = "none";
+    }
 
     timeDiv.textContent =
         addZero(hours) + ":" + addZero(minutes) + ":" + addZero(seconds);
@@ -24,7 +69,7 @@ function addZero(time) {
 
 // Validation part
 
-validateHours = () => {
+validateHours = (inputedHours) => {
     const hours = inputedHours.value;
     inputedHours.value = Math.round(hours);
     if (hours > 23) {
@@ -33,7 +78,12 @@ validateHours = () => {
         inputedHours.value = 0;
     }
 
-    setTimeButton.disabled = false;
+    if (inputedHours.id === "hours") {
+        setTimeButton.disabled = false;
+    }
+    if (inputedHours.id === "alarmHours") {
+        alarmCreateBtn.disabled = false;
+    }
 };
 validateMinutesAndSeconds = (input) => {
     let value = input.value;
@@ -43,17 +93,31 @@ validateMinutesAndSeconds = (input) => {
     } else if (value < 0) {
         input.value = 0;
     }
-    setTimeButton.disabled = false;
+    if (input.id === "minutes" || input.id === "seconds") {
+        setTimeButton.disabled = false;
+    }
+    if (input.id === "alarmMinutes" || input.id === "alarmSeconds") {
+        alarmCreateBtn.disabled = false;
+    }
 };
 
 // ON INPUT
 
-inputedHours.addEventListener("input", validateHours);
+inputedHours.addEventListener("input", () => validateHours(inputedHours));
 inputedMinutes.addEventListener("input", () =>
     validateMinutesAndSeconds(inputedMinutes)
 );
 inputedSeconds.addEventListener("input", () =>
     validateMinutesAndSeconds(inputedSeconds)
+);
+
+// ON ALRM INPUT
+alarmHours.addEventListener("input", () => validateHours(alarmHours));
+alarmMinutes.addEventListener("input", () =>
+    validateMinutesAndSeconds(alarmMinutes)
+);
+alarmSeconds.addEventListener("input", () =>
+    validateMinutesAndSeconds(alarmSeconds)
 );
 
 // Validation part end
@@ -82,6 +146,19 @@ handleStartClick = () => {
         if (inputedHours === 24) {
             inputedHours = 0;
         }
+
+        // Clock time
+        if (
+            createdAlarmTime ===
+            addZero(inputedHours) +
+                ":" +
+                addZero(inputedMinutes) +
+                ":" +
+                addZero(inputedSeconds)
+        ) {
+            alarmIsRingingImageContainer.style.display = "block";
+            alarmIsSetImageContainer.style.display = "none";
+        }
         // time changing part ends here
         timeDiv.textContent =
             addZero(inputedHours) +
@@ -90,11 +167,29 @@ handleStartClick = () => {
             ":" +
             addZero(inputedSeconds);
     }, 1000);
+};
+// HANDLE ALARM CLICK
 
-    function addZero(time) {
-        return time < 10 ? "0" + time : time;
+handleAlarmCreateClick = () => {
+    if (removeSpace(alarmName.value) === "") {
+        alarmName.placeholder = ALERT_TEXT_REQUIRED;
+        return "";
+    }
+    alarmIsSetImageContainer.style.display = "block";
+    if (!createdAlarmTime) {
+        createdAlarmTime =
+            addZero(alarmHours.value) +
+            ":" +
+            addZero(alarmMinutes.value) +
+            ":" +
+            addZero(alarmSeconds.value);
+        alarmTime.textContent = createdAlarmTime + " " + alarmName.value;
+    } else {
+        alertText.textContent = ALERT_TEXT;
     }
 };
 
 setTimeButton.disabled = true;
+alarmCreateBtn.disabled = true;
 setTimeButton.addEventListener("click", handleStartClick);
+alarmCreateBtn.addEventListener("click", handleAlarmCreateClick);
