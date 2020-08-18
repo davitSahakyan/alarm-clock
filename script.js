37ab09348db101b592b0cdf3b755b45bc4e44976
Davit Sahakyan <sahakyandavit92@gmail.com>
2020-08-18 14:25:29 +0400

    Add validation for time input part

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const timeDiv = document.getElementById("time");
 
+let inputedHours = document.getElementById("hours");
+let inputedMinutes = document.getElementById("minutes");
+let inputedSeconds = document.getElementById("seconds");
+
 let currentTime = setInterval(() => {
-    var date = new Date();
+    let date = new Date();
 
-    var hours = date.getHours();
+    let hours = date.getHours();
 
-    var minutes = date.getMinutes();
+    let minutes = date.getMinutes();
 
-    var seconds = date.getSeconds();
+    let seconds = date.getSeconds();
 
     timeDiv.textContent =
         addZero(hours) + ":" + addZero(minutes) + ":" + addZero(seconds);
@@ -16,3 +20,45 @@ let currentTime = setInterval(() => {
 function addZero(time) {
     return time < 10 ? "0" + time : time;
 }
+
+// Validation part
+
+validateHours = () => {
+    const hours = inputedHours.value;
+    inputedHours.value = Math.round(hours);
+    if (hours > 24) {
+        inputedHours.value = 24;
+    } else if (hours < 0) {
+        inputedHours.value = 0;
+    }
+};
+validateMinutesAndSeconds = (input) => {
+    let value = input.value;
+    input.value = Math.round(value);
+    if (value > 60) {
+        input.value = 60;
+    } else if (value < 0) {
+        input.value = 0;
+    }
+};
+
+inputedHours.addEventListener("input", validateHours);
+inputedMinutes.addEventListener("input", () =>
+    validateMinutesAndSeconds(inputedMinutes)
+);
+inputedSeconds.addEventListener("input", () =>
+    validateMinutesAndSeconds(inputedSeconds)
+);
+
+// Validation part end
+
+handleStartClick = () => {
+    let inputedHours = document.getElementById("hours").value;
+    let inputedMinutes = document.getElementById("minutes").value;
+    let inputedSeconds = document.getElementById("seconds").value;
+
+    console.log(inputedHours, inputedMinutes, inputedSeconds);
+};
+
+const setTimeButton = document.getElementById("setTimeButton");
+setTimeButton.addEventListener("click", handleStartClick);
